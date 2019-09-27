f03d6314f9e6ebeae6035f81f592a8b511ed8d2e
Joakim Isaksson <joakim.isaksson@spinit.se>
2019-09-27 08:42:27 +0200

    changes in social media section, choose 4 images in the plugin ---> feed them

--- src/components/social_media.php
+++ src/components/social_media.php
@@ -2,8 +2,11 @@
     <div class="container">
         <h2>Instagram</h2>
 
+        <div class="social-media-grid">
         <?php 
             the_content();
         ?>
+        </div>
+
     </div>
 </div>
