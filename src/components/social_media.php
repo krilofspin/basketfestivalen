b652749cedf473428c441a658b4b91c4f131f9f0
Viktor Richardson <viktor.richardson@spinit.se>
2019-10-11 12:47:19 +0200

    sections and partners spacing

--- src/components/social_media.php
+++ src/components/social_media.php
@@ -2,7 +2,9 @@
 $social_media = get_field("social_media");
 ?>
 <div class="container">
-    <div class="social-media">
-        <?=$social_media;?>
+    <div clas="section">
+        <div class="social-media">
+            <?=$social_media;?>
+        </div>
     </div>
 </div>
