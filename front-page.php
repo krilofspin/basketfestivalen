61cfcefdf9375818976fc5a80bf976755fc35349
Kristian Löfvenholm <kristian.lofvenholm@spinit.se>
2019-10-14 11:50:25 +0200

    Add spacing to footer for Skyline, larger logo, hide social-media template

--- front-page.php
+++ front-page.php
@@ -2,7 +2,15 @@
 <?php 
 get_header(); 
 
-$components = ["top_menu", "hero", "main_content", "about", "apps", /*"news",*/ "social_media"];
+$components = [
+    "top_menu", 
+    "hero", 
+    "main_content", 
+    "about", 
+    "apps", 
+    // "news",
+    // "social_media"
+];
 
 foreach ($components as $component) {
     get_template_part( "src/components/" . $component);
--- src/components/apps.php
+++ src/components/apps.php
@@ -1,17 +1,22 @@
 <?php 
+$apps_image = get_field("apps_image");
 $apps_text = get_field("apps_text");
 $apps_logos = get_field("apps_logos");
 ?>
-<div id="about" class="section">
+<div id="apps" class="section">
     <div class="container">
         <div class="columns align-center">
+            <? if (!empty($apps_image)) {?>
+                <div class="column">
+                    <img src="<?=$apps_image;?>" alt="" />           
+                </div>
+            <? } ?>
+
             <div class="column">
-                <p class="h4"><?php echo nl2br($apps_text);?></p>                
-            </div>
-            <div class="column">
+                <p class="h4"><?php echo nl2br($apps_text);?></p>
                 <? foreach ($apps_logos as $logos) {?>
                     <a href="<?=$logos["apps_link_to_store"];?>">
-                        <img src="<?=$logos["apps_logos_image"]["url"];?>" alt="<?=$logos["apps_logos_image"]["title"];?>" width="250" />
+                        <img src="<?=$logos["apps_logos_image"]["url"];?>" alt="<?=$logos["apps_logos_image"]["title"];?>" width="190" />
                     </a><br/>
                 <? } ?>
             </div>
