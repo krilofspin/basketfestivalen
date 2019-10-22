9d0dfa5b9a92c1900736a7866d6ca621118ffc53
Kristian Löfvenholm <kristian.lofvenholm@spinit.se>
2019-10-22 09:07:08 +0200

    Use https in header

--- header.php
+++ header.php
@@ -11,13 +11,13 @@ if (is_admin_bar_showing()) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta property="og:url" content="http://basketballfestival.se"/>
+  <meta property="og:url" content="https://basketballfestival.se"/>
   <meta property="og:type" content="website" />
   <meta property="og:title" content="Basketfestivalen" />
   <meta property="og:description" content="" />
-  <meta property="og:image:secure_url" content="http://basketballfestival.se/wp-content/themes/basketfestivalen/screenshot.png" />
+  <meta property="og:image:secure_url" content="https://basketballfestival.se/wp-content/themes/basketfestivalen/screenshot.png" />
   <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/dist/css/main.bundle.css">
-  <link rel="icon" href="http://basketballfestival.se/wp-content/uploads/sport_basketball_favicon.png"/>
+  <link rel="icon" href="https://basketballfestival.se/wp-content/uploads/sport_basketball_favicon.png"/>
   <title><?= the_title(); ?> - <?= get_bloginfo(); ?></title>
   <?php wp_head() ?>
 </head>
